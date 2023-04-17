74f1a12e2b13abf31a22a7600d2abc17977e92ee
Flo Kempenich <flo@kempenich.dev>
2023-04-17 11:53:35 +0100

    Implement DB wrapper

--- src/things3/db.py
+++ src/things3/db.py
@@ -1,20 +1,72 @@
 import sqlite3
 from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
 from pathlib import Path
 
-from src.things3.hierarchy import Area, Tag
+from src.things3.hierarchy import Area, Tag, Task, Status, Project, Heading
+from src.utils import convert_bin_date
 
+
+class Type(Enum):
+    TASK = 0
+    PROJECT = 1
+    HEADING = 2
+
+
+# Tag Queries
 SQL_FETCH_TAG = "select title from TMTag where uuid = ?"
-
-SQL_FETCH_AREA_TAGS = """select tag.title, tag.parent from TMArea area
+SQL_AREA_FETCH_TAGS = """select tag.title, tag.parent from TMArea area
                         join TMAreaTag areaTag on areaTag.areas = area.uuid
                         join TMTag tag on tag.uuid = areaTag.tags
                         where area.uuid = ?"""
+SQL_TASK_OR_PROJECT_FETCH_TAGS = """select tag.title, tag.parent from TMTask task
+                        join TMTaskTag taskTag on taskTag.tasks = task.uuid
+                        join TMTag tag on tag.uuid = taskTag.tags
+                        where task.uuid = ?"""
 
-FETCH_EVERYTHING_SQL = """
-SELECT * FROM TMTask
-where area = ?
+# IDs Queries
+SQL_AREA_FETCH_TASK_IDS = f"""select uuid 
+                                    from TMTask task 
+                                    where task.area = ? 
+                                        and task.type = {Type.TASK.value}
+                                        and trashed = 0
+                                    ORDER BY task."index" """
+SQL_AREA_FETCH_PROJECT_IDS = f"""select uuid 
+                                        from TMTask project 
+                                        where project.area = ? 
+                                            and project.type = {Type.PROJECT.value}
+                                            and trashed = 0
+                                        ORDER BY project."index" """
+
+SQL_PROJECT_FETCH_TASK_IDS = f"""select uuid 
+                                    from TMTask task 
+                                    where task.project = ?
+                                        and task.type = {Type.TASK.value}
+                                        and trashed = 0
+                                    ORDER BY task."index" """
+SQL_PROJECT_FETCH_HEADING_IDS = f"""select uuid 
+                                    from TMTask heading
+                                    where heading.project = ?
+                                        and heading.type = {Type.HEADING.value}
+                                        and trashed = 0
+                                    ORDER BY heading."index" """
+SQL_HEADING_FETCH_TASK_IDS = f"""select uuid    
+                                        from TMTask task 
+                                        where task.heading = ?
+                                            and task.type = {Type.TASK.value}
+                                            and trashed = 0
+                                        ORDER BY task."index" """
+
+# Item Queries
+SQL_FETCH_PROJECT_OR_HEADING_OR_TASK = """
+SELECT title, notes, status, deadline, startDate, start, rt1_recurrenceRule, stopDate 
+FROM TMTask
+where uuid = ?
 """
+SQL_FETCH_CHECKLIST_ITEMS = (
+    'select title from TMChecklistItem where task == ? order by "index"'
+)
 
 
 @dataclass
@@ -24,6 +76,19 @@ class AreaInfo:
 
 
 class DB:
+    """
+    A wrapper around the Things 3 SQLite database.
+
+    NOT SUPPORTED:
+        - Trashed items
+        - Projects NOT in an Area
+        - Repeating schedule on repeating tasks
+        (only supports flagging the task as repeating)
+
+    SUPPORTED:
+        - Everything Else
+    """
+
     def __init__(self, db_path: Path):
         print(db_path.absolute())
         self.conn = sqlite3.connect(str(db_path))
@@ -45,7 +110,7 @@ class DB:
 
         def fetch_area_tags():
             tags = []
-            for tag_row in self.conn.execute(SQL_FETCH_AREA_TAGS, (area_uuid,)):
+            for tag_row in self.conn.execute(SQL_AREA_FETCH_TAGS, (area_uuid,)):
                 tag = Tag(tag_row[0])
                 if parent_tag := tag_row[1]:
                     tag.parent = Tag(
@@ -57,10 +122,24 @@ class DB:
             return tags
 
         def fetch_projects():
-            return []
+            project_ids = [
+                row[0]
+                for row in self.conn.execute(
+                    SQL_AREA_FETCH_PROJECT_IDS, (area_uuid,)
+                )
+            ]
+            return [
+                self.fetch_project(project_id) for project_id in project_ids
+            ]
 
         def fetch_tasks():
-            return []
+            task_ids = [
+                row[0]
+                for row in self.conn.execute(
+                    SQL_AREA_FETCH_TASK_IDS, (area_uuid,)
+                )
+            ]
+            return [self.fetch_task(task_id) for task_id in task_ids]
 
         return Area(
             fetch_title(),
@@ -68,3 +147,136 @@ class DB:
             projects=fetch_projects(),
             tasks=fetch_tasks(),
         )
+
+    def fetch_task(self, task_id):
+        def fetch_tags():
+            tags = []
+            for tag_row in self.conn.execute(
+                SQL_TASK_OR_PROJECT_FETCH_TAGS, (task_id,)
+            ):
+                tag = Tag(tag_row[0])
+                if parent_tag := tag_row[1]:
+                    tag.parent = Tag(
+                        self.conn.execute(
+                            SQL_FETCH_TAG, (parent_tag,)
+                        ).fetchone()[0]
+                    )
+                tags.append(tag)
+            return tags
+
+        def fetch_checklist_items():
+            return [
+                Task(row[0])
+                for row in self.conn.execute(
+                    SQL_FETCH_CHECKLIST_ITEMS, (task_id,)
+                )
+            ]
+
+        row = self.conn.execute(
+            SQL_FETCH_PROJECT_OR_HEADING_OR_TASK, (task_id,)
+        ).fetchone()
+
+        title = row[0]
+        note = row[1]
+        status = row[2]
+        deadline = row[3]
+        start_date = row[4]
+        start = row[5]
+        recurrence_rule = row[6]
+        stop_date = row[7]
+
+        return Task(
+            title,
+            note,
+            tags=fetch_tags(),
+            checklist=fetch_checklist_items(),
+            status=Status(status),
+            due_date=convert_bin_date(deadline) if deadline else None,
+            defer_date=convert_bin_date(start_date) if start_date else None,
+            someday=start == 2 and not start_date and recurrence_rule is None,
+            repeating=recurrence_rule is not None,
+            completion_datetime=(
+                datetime.fromtimestamp(stop_date) if stop_date else None
+            ),
+        )
+
+    def fetch_project(self, project_id):
+        def fetch_tags():
+            tags = []
+            for tag_row in self.conn.execute(
+                SQL_TASK_OR_PROJECT_FETCH_TAGS, (project_id,)
+            ):
+                tag = Tag(tag_row[0])
+                if parent_tag := tag_row[1]:
+                    tag.parent = Tag(
+                        self.conn.execute(
+                            SQL_FETCH_TAG, (parent_tag,)
+                        ).fetchone()[0]
+                    )
+                tags.append(tag)
+            return tags
+
+        def fetch_tasks():
+            task_ids = [
+                row[0]
+                for row in self.conn.execute(
+                    SQL_PROJECT_FETCH_TASK_IDS, (project_id,)
+                )
+            ]
+            return [self.fetch_task(task_id) for task_id in task_ids]
+
+        def fetch_headings():
+            heading_ids = [
+                row[0]
+                for row in self.conn.execute(
+                    SQL_PROJECT_FETCH_HEADING_IDS, (project_id,)
+                )
+            ]
+            return [
+                self.fetch_heading(heading_id) for heading_id in heading_ids
+            ]
+
+        row = self.conn.execute(
+            SQL_FETCH_PROJECT_OR_HEADING_OR_TASK, (project_id,)
+        ).fetchone()
+
+        title = row[0]
+        note = row[1]
+        status = row[2]
+        deadline = row[3]
+        start_date = row[4]
+        start = row[5]
+        repeating_template = row[6]
+        stop_date = row[7]
+
+        return Project(
+            title,
+            note,
+            tags=fetch_tags(),
+            tasks=fetch_tasks(),
+            headings=fetch_headings(),
+            status=Status(status),
+            due_date=convert_bin_date(deadline) if deadline else None,
+            defer_date=convert_bin_date(start_date) if start_date else None,
+            someday=start == 2 and not start_date,
+            repeating=repeating_template is not None,
+            completion_datetime=(
+                datetime.fromtimestamp(stop_date) if stop_date else None
+            ),
+        )
+
+    def fetch_heading(self, heading_id):
+        def fetch_tasks():
+            task_ids = [
+                row[0]
+                for row in self.conn.execute(
+                    SQL_HEADING_FETCH_TASK_IDS, (heading_id,)
+                )
+            ]
+            return [self.fetch_task(task_id) for task_id in task_ids]
+
+        row = self.conn.execute(
+            SQL_FETCH_PROJECT_OR_HEADING_OR_TASK, (heading_id,)
+        ).fetchone()
+
+        return Heading(title=row[0], tasks=fetch_tasks())
--- tests/things3/test_db.py
+++ tests/things3/test_db.py
@@ -1,9 +1,10 @@
+from datetime import date, datetime
 from pathlib import Path
 
 import pytest
 
 from src.things3.db import DB, AreaInfo
-from src.things3.hierarchy import Area, Tag
+from src.things3.hierarchy import Area, Tag, Task, Status, Project, Heading
 
 AREA_ID_THE_AREA = "Dr6Ji5SN5eyuDWzKxK9WL9"
 
@@ -38,6 +39,138 @@ def test_area_with_everything(db):
         tags=[
             Tag("Area Tag Child", parent=Tag("Area Tag Parent")),
         ],
-        projects=[],
-        tasks=[],
+        projects=[
+            Project(
+                "Project In The Area",
+                note="Some project note",
+                tags=[Tag("Project Tag Parent")],
+                due_date=date(2023, 7, 14),
+                tasks=[
+                    Task(
+                        "Task in Project In The Area",
+                        note="Some Task note",
+                        tags=[Tag("Task Tag Parent")],
+                    ),
+                    Task(
+                        "Task w/ Deadline in Project In The Area",
+                        due_date=date(2023, 6, 14),
+                        tags=[Tag("Task Tag Parent")],
+                    ),
+                    Task(
+                        "Task w/ Checklist in Project In The Area",
+                        checklist=[
+                            Task("Checklist item 1"),
+                            Task("Checklist item 2"),
+                            Task("Checklist item 3"),
+                        ],
+                        tags=[
+                            Tag("Task Tag Child", parent=Tag("Task Tag Parent"))
+                        ],
+                    ),
+                    Task(
+                        "Dropped Task in Project In The Area",
+                        status=Status.DROPPED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 14, 472046
+                        ),
+                    ),
+                    Task(
+                        "Completed Task in Project In The Area",
+                        status=Status.COMPLETED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 7, 466055
+                        ),
+                    ),
+                    Task(
+                        "Deferred Task in Project In The Area",
+                        defer_date=date(2024, 1, 1),
+                    ),
+                    Task("Someday Task in Project In The Area", someday=True),
+                    Task(
+                        "Repeating Task in Project In The Area",
+                        repeating=True,
+                    ),
+                ],
+                headings=[
+                    Heading(
+                        "Heading",
+                        tasks=[
+                            Task("Task in Heading"),
+                            Task("Another Task in Heading"),
+                        ],
+                    )
+                ],
+            ),
+            Project(
+                "Dropped In The Area",
+                status=Status.DROPPED,
+                completion_datetime=datetime(2023, 4, 14, 10, 39, 8, 309116),
+                tasks=[
+                    Task(
+                        "Dropped Task in Project In The Area",
+                        status=Status.DROPPED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 14, 472046
+                        ),
+                    ),
+                    Task(
+                        "Completed Task in Project In The Area",
+                        status=Status.COMPLETED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 7, 466055
+                        ),
+                    ),
+                ],
+            ),
+            Project(
+                "Completed Project In The Area",
+                status=Status.COMPLETED,
+                completion_datetime=datetime(2023, 4, 14, 10, 38, 56, 889877),
+                tasks=[
+                    Task(
+                        "Dropped Task in Project In The Area",
+                        status=Status.DROPPED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 14, 472046
+                        ),
+                    ),
+                    Task(
+                        "Completed Task in Project In The Area",
+                        status=Status.COMPLETED,
+                        completion_datetime=datetime(
+                            2023, 4, 14, 10, 37, 7, 466055
+                        ),
+                    ),
+                ],
+            ),
+            Project(
+                "Someday Project In The Area",
+                someday=True,
+                tasks=[
+                    Task("Task in Someday Project In The Area"),
+                ],
+            ),
+            Project(
+                "Deferred Project In The Area",
+                defer_date=date(2024, 1, 1),
+                tasks=[
+                    Task("Task in Deferred Project In The Area"),
+                ],
+            ),
+        ],
+        tasks=[
+            Task("Task in the Area"),
+            Task(
+                "Completed Task in the Area",
+                status=Status.COMPLETED,
+                completion_datetime=datetime(2023, 4, 14, 10, 40, 18, 94449),
+            ),
+            Task(
+                "Dropped Task in the Area",
+                status=Status.DROPPED,
+                completion_datetime=datetime(2023, 4, 14, 10, 40, 21, 182182),
+            ),
+            Task("Deferred Task in the Area", defer_date=date(2024, 1, 1)),
+            Task("Someday Task in the Area", someday=True),
+        ],
     )
